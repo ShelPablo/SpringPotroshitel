92fd5502bcf1b45ec3c52f45a50a97f03a72169c
Shel <Shel-ne@mail.ru>
2018-09-03 06:30:18 +0300

    Added init method

--- src/main/java/quater/TerminatorQuater.java
+++ src/main/java/quater/TerminatorQuater.java
@@ -3,7 +3,21 @@ package quater;//package quaters;
 
 import quater.Quater;
 
+import javax.annotation.PostConstruct;
+
 public class TerminatorQuater implements Quater {
+
+    public TerminatorQuater() {
+        System.out.println("Phase 1");
+    }
+
+    @PostConstruct
+    public void init()
+    {
+        System.out.println("Phase 2");
+        System.out.println(repeat);
+    }
+
 
     @InjectRandomInt(min = 2, max = 7)
     private int repeat;

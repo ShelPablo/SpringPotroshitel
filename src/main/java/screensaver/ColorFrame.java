b949d3798bdd0e87b3ec4da43e94a4c571ecf7a3
Shel <Shel-ne@mail.ru>
2018-09-05 16:14:58 +0300

    Dynamic change of singleton field (screensaver)

---
+++ src/main/java/screensaver/ColorFrame.java
@@ -0,0 +1,30 @@
+package screensaver;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.Random;
+
+@Component
+public abstract class ColorFrame extends JFrame {
+    @Autowired
+    private Color color;
+
+    public ColorFrame() throws HeadlessException {
+            setSize(200, 200);
+            setVisible(true);
+            setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    }
+
+    public void showOnRandomPlace(){
+        Random random = new Random();
+        setLocation(random.nextInt(1200), random.nextInt(600));
+        getContentPane().setBackground(getColor());
+        repaint();
+
+    }
+
+    protected abstract Color getColor();
+}
